25ada9d0df79968cec60c889ecb489a3b55e9499
Kajetan Swiatek <kajetan.swiatek@gmail.com>
2025-05-28 14:17:27 +0200

    feat: SearBar is now connected with redux store; changing searchbar value is debounced to prevent too many requests

--- src/routes/home/components/SearchBar/SearchBar.tsx
+++ src/routes/home/components/SearchBar/SearchBar.tsx
@@ -1,23 +1,38 @@
 import { useDispatch, useSelector } from "react-redux";
 import { filtersActions, filtersSelectors } from "@/store/features/filters/filtersSlice";
+import { useCallback, useEffect, useRef } from "react";
+import { debounce } from "lodash";
 
 import "./SearchBar.scss";
 
 export const SearchBar = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   const searchQuery = useSelector(filtersSelectors.selectQuery);
   const dispatch = useDispatch();
+
+  const setSearchQuery = useCallback(
+    debounce((query: string) => {
+      dispatch(filtersActions.setQuery(query));
+    }, 500),
+    [],
+  );
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.value = searchQuery;
+    }
+  }, []);
 
   return (
     <div className="container">
       <input
         className="searchInput"
+        ref={inputRef}
         type="text"
         placeholder="Search for movies..."
         aria-label="Search for movies..."
-        value={searchQuery}
-        onChange={(e) => {
-          dispatch(filtersActions.setQuery(e.target.value));
-        }}
+        onChange={(e) => setSearchQuery(e.target.value)}
       />
     </div>
   );
