6aaeff95612798d5a6bcda10249703f9b13be2d6
Kajetan Swiatek <kajetan.swiatek@gmail.com>
2025-05-28 14:32:20 +0200

    feat: base implementation for fetching and displaying simple list of movies

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -1,19 +1,13 @@
-import { useGetMoviesByQueryQuery } from "@/store/services/movies";
 import { SearchBar } from "./components/SearchBar/SearchBar";
+import { MoviesList } from "./components/MoviesList/MoviesList";
 
 import "./Home.scss";
 
 export default function Home() {
-  const { data } = useGetMoviesByQueryQuery({
-    page: 1,
-    query: "blade",
-  });
-
-  console.log(data);
-
   return (
     <>
-      <SearchBar></SearchBar>
+      <SearchBar />
+      <MoviesList />
     </>
   );
 }
---
+++ src/routes/home/components/MoviesList/MoviesList.tsx
@@ -0,0 +1,43 @@
+import { useGetMoviesByQueryQuery } from "@/store/services/movies";
+import { isEmpty } from "lodash";
+import { useSelector } from "react-redux";
+import { filtersSelectors } from "@/store/features/filters/filtersSlice";
+
+import "./MoviesList.scss";
+
+export const MoviesList = () => {
+  const searchQuery = useSelector(filtersSelectors.selectQuery);
+
+  const movies = useGetMoviesByQueryQuery(
+    {
+      page: 1,
+      query: searchQuery,
+    },
+    {
+      skip: !searchQuery,
+    },
+  );
+
+  if (movies.isUninitialized) {
+    // TODO implement Idle state
+    return <div>Type your phrase</div>;
+  }
+
+  if (movies.isFetching) {
+    // TODO implement loading state
+    return <div>Loading...</div>;
+  }
+
+  if (isEmpty(movies.data?.results)) {
+    // TODO implement No Results state
+    return <div>No results!</div>;
+  }
+
+  return (
+    <ul>
+      {movies.data?.results.map((movie) => (
+        <li key={movie.id}>{movie.title}</li>
+      ))}
+    </ul>
+  );
+};
